26d049351129266d438d9debe17e1f65d94fc778
kajal1801 <anshikajais2001@gmail.com>
2022-11-23 23:47:45 +0530

    Added some css

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const { userProfile } = require("./models/username.js");
 const app = express();
 const port = 3000 || process.env.PORT;
+const path=require('path')
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "views")));
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -12,8 +14,11 @@ app.get("/crysosancher", async (req, res) => {
   //console.log(req.params);
   //res.render('username',{username:req.params.username});
   const users = [
-    { username: "crysosancher", name: "Vibhu Pandey", Enrollment: "2011092" },
-    { username: "kajal1801", name: "Kajal jaiswal", Enrollment: "2011069" },
+    { username: "crysosancher", name: "Vibhu Pandey", Enrollment: "1092" },
+    { username: "kajal1801", name: "Kajal Jaiswal", Enrollment: "1076" },
+    { username: "isha", name: "Isha Khannna", Enrollment: "1075" },
+    { username: "ChickyWings01", name: "Kaninika Sen Choudhuri", Enrollment: "1077" },
+    { username: "Ipsita", name: "Ipsita Sanyashi", Enrollment: "1074" },
   ];
   let profileDataArray=[];
 for(let i=0;i<users.length;i++){
